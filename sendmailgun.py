8d8277ea0e0afa260bc55270058bb58eae980cfb
Tyler Kraft <tkraft@Tylers-MBP.hillsidewired.com>
2019-04-25 11:40:57 -0500

    testing mailgun data to pass

--- sendmailgun.py
+++ sendmailgun.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 
 import requests
+import csv
 
-baseurl = 'https://api.mailgun.net/v2/rock.hillsidewired.com'
+baseurl = 'https://api.mailgun.net/v2/mailgun.example.com/messages'
 
+auth = ('api', 'YOUR MAILGUN API KEY')
 
+emailfrom = 'John Smith <johnsmith@example.com>'
 
+emailsubject = 'YOUR SUBJECT'
+
+emailtext = """
+YOUR TEXT
+ """
+
+with open('file.txt') as f:
+    for line in f:
+
+    
+    data = {
+        'from': emailfrom,
+        'to': line,
+        'subject': emailsubject,
+        'text': emailtext
+        }
+
+    print(data)
